1734b7a9c34288cda9b95668308ec18f109d6f53
ErlensGadzans <erlens.gadzans@users.noreply.github.com>
2020-12-08 18:03:10 +0200

    Update index.js

--- src/users/index.js
+++ src/users/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const uniqid = require("uniqid");
+
 const readDatabase = () => {
   const usersFilePath = path.join(__dirname, "users.json");
   const fileAsABuffer = fs.readFileSync(usersFilePath);
@@ -16,14 +21,20 @@ router.get("/", (req, res) => {
 
 router.get("/:id", (req, res) => {
   const usersArray = readDatabase();
+  const idComingFromRequest = req.params.identifier;
+  const user = usersArray.filter((user) => user.ID === idComingFromRequest);
 });
 
 router.post("/", (req, res) => {
   const usersArray = readDatabase();
+  const newUserID = addNewUser(req.body);
+  res.status(201).send({ id: newUserID });
 });
 
 router.put("/:id", (req, res) => {
   const usersArray = readDatabase();
+  const updatedUser = updateUser(req.params.id, req.body);
+  res.send(updatedUser);
 });
 
 router.delete("/:id", (req, res) => {
